1aa973b8268e347b0cb66d283a969bc1cdfdfeb6
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-09-14 16:26:29 +0300

    :star: first request to the Unsplash

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,21 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 function App() {
   const [term, setTerm] = useState(" ");
 
   const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log(term);
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${term}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+
+    setTerm("");
   };
 
   return (

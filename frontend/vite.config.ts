f33397fd1176c3a3dac235f4fd51024b6e44dec8
Olga Prykhodko <helga.prikhodko@gmail.com>
2023-09-14 12:49:48 +0300

    🔧 solve issue "process not found"

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+
+const cherryPickedKeys = ["NODE_ENV", "FAST_REFRESH", "REACT_APP_UNSPLASH_KEY"];
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  const processEnv = {};
+  cherryPickedKeys.forEach((key) => (processEnv[key] = env[key]));
+
+  return {
+    define: {
+      "process.env": processEnv,
+    },
+    plugins: [react()],
+  };
+});
